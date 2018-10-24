e5c11767995ffca6fe5dbc69f41061d9341a51cc
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-24 22:34:44 +0900

    some changse

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import main.scala.core.LexicalAnalyzerImpl
+import main.scala.core.{EOF, LexicalAnalyzerImpl}
 
 import scala.io.Source
 
@@ -6,7 +6,13 @@ object Main extends App {
   try {
     val SORTH_PATH = "test1.bas"
     val source = Source.fromFile(SORTH_PATH)
-    LexicalAnalyzerImpl(source)
+    val lexicalAnalyzer =  new LexicalAnalyzerImpl(SORTH_PATH)
+
+    source.map { c =>
+      var lexicalUnit = lexicalAnalyzer.get()
+      if(lexicalUnit.getType == EOF) return
+      println(lexicalUnit)
+    }
     source.foreach(c => print(c))
   } catch {
     case e: Exception => println("io error occured.")
--- src/main/scala/core/LexicalUnit.scala
+++ src/main/scala/core/LexicalUnit.scala
@@ -1,45 +1,48 @@
 package main.scala.core
 
-class LexicalUnit(val lexicalType: Option[LexicalType], val value: Option[Value]) {
+class LexicalUnit(val lexicalType: LexicalType, val value: Option[Value]) {
+
+  def getType: Option[Value] = return value
+  def getValue: LexicalType = return lexicalType
 
   def toString(): String = {
     type match {
       case LITERAL => "LITERAL:\t" + value.map(_.getString);
-	    case NAME => "NAME:\t" + value.map(_.getString);
-	    case DOUBLEVAL => "DOUBLEVAL:\t" + value.map(_.getString);
-	    case INTVAL => "INTVAL:\t" + value.map(_.getString);
-	    case IF => "IF"
-	    case THEN => "THEN"
-	    case ELSE => "ELSE"
-	    case FOR => "FOR"
-	    case FORALL => "FORALL"
-	    case NEXT => "NEXT"
-	    case SUB => "SUB"
-	    case DIM => "DIM"
-	    case AS => "AS"
-	    case END => "END"
-	    case EOF => "EOF"
-	    case NL => "NL"
-	    case EQ => "EQ"
-	    case LT => "LT"
-	    case GT => "GT"
-	    case LE => "LE"
-	    case GE => "GE"
-	    case DOT => "DOT"
-	    case WHILE => "WHILE"
-	    case UNTIL => "UNTIL"
-	    case ADD => "ADD"
-	    case MUL => "MUL"
-	    case DIV => "DIV"
-	    case LP => "LP"
-	    case RP => "RP"
-	    case COMMA => "COMMA"
-	    case LOOP => "LOOP"
-	    case TO => "TO"
-	    case WEND => "WEND"
-	    case ELSEIF => "ELSEIF"
-	    case NE => "NE"
-	    case ENDIF => "ENDIF"
+      case NAME => "NAME:\t" + value.map(_.getString);
+      case DOUBLEVAL => "DOUBLEVAL:\t" + value.map(_.getString);
+      case INTVAL => "INTVAL:\t" + value.map(_.getString);
+      case IF => "IF"
+      case THEN => "THEN"
+      case ELSE => "ELSE"
+      case FOR => "FOR"
+      case FORALL => "FORALL"
+      case NEXT => "NEXT"
+      case SUB => "SUB"
+      case DIM => "DIM"
+      case AS => "AS"
+      case END => "END"
+      case EOF => "EOF"
+      case NL => "NL"
+      case EQ => "EQ"
+      case LT => "LT"
+      case GT => "GT"
+      case LE => "LE"
+      case GE => "GE"
+      case DOT => "DOT"
+      case WHILE => "WHILE"
+      case UNTIL => "UNTIL"
+      case ADD => "ADD"
+      case MUL => "MUL"
+      case DIV => "DIV"
+      case LP => "LP"
+      case RP => "RP"
+      case COMMA => "COMMA"
+      case LOOP => "LOOP"
+      case TO => "TO"
+      case WEND => "WEND"
+      case ELSEIF => "ELSEIF"
+      case NE => "NE"
+      case ENDIF => "ENDIF"
     }
   }
 }
