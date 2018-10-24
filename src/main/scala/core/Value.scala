905f162931b6582e0872f9dbf9878f6a8087de6a
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-24 13:31:08 +0900

    add impl for Value

--- src/main/scala/core/Value.scala
+++ src/main/scala/core/Value.scala
@@ -6,3 +6,19 @@ trait Value {
   def getBoolean: Boolean
   def getType:ValueType
 }
+
+class ValueImpl(value: String, valueType: ValueType) extends Value {
+
+  def this(s: String) = this(s, STRING)
+  def this(i: Int) = this(i.toString, INTEGER)
+  def this(d: Double) = this(d.toString, DOUBLE)
+  def this(b: Boolean) = this(b.toString, BOOL)
+
+
+  def getString: String = value
+  def getInt: Int = value.toInt
+  def getDouble: Double = value.toDouble
+  def getBoolean: Boolean = value.toBoolean
+
+  def getType: ValueType = valueType
+}
