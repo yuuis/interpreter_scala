91da793b3c961c2714b2a5d4030b8f7413e49d49
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-04 20:07:12 +0900

    fix typo and use break

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,17 @@
 import main.scala.core.{EOF, LexicalAnalyzerImpl}
 
 import scala.io.Source
+import util.control.Breaks._
 
 object Main extends App {
   try {
-    val SORTH_PATH = "test1.bas"
-    val source = Source.fromFile(SORTH_PATH)
-    val lexicalAnalyzer =  new LexicalAnalyzerImpl(SORTH_PATH)
+    val SOURCE_PATH = "test1.bas"
+    val source = Source.fromFile(SOURCE_PATH)
+    val lexicalAnalyzer =  new LexicalAnalyzerImpl(SOURCE_PATH)
 
     source.map { c =>
-      var lexicalUnit = lexicalAnalyzer.get()
-      if(lexicalUnit.getType == EOF) return
+      val lexicalUnit = lexicalAnalyzer.get()
+      if(lexicalUnit.getType == EOF) break()
       println(lexicalUnit)
     }
     source.foreach(c => print(c))
