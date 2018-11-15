28fe162ce3ed2f27ab9b51e76fceb6718b8d9d98
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-15 22:09:06 +0900

    add Node class

--- src/main/scala/core/Node.scala
+++ src/main/scala/core/Node.scala
@@ -1,5 +1,15 @@
 package main.scala.core
 
-class Node {
+class Node(val nodeType: Option[NodeType], val environment: Option[Environment]) {
+  def this(nodeType: NodeType) = this(Some(nodeType), None)
+  def this(environment: Environment) = this(None, Some(environment))
 
+  def getType() = nodeType
+  def Parse(): Boolean = true
+  def toString = {
+    nodeType match {
+      case NodeType.END => "END"
+      case _ => "Node"
+    }
+  }
 }
