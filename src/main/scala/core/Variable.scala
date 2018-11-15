3d4809ae8dcc23d0c1c456d599f8bc6c6776e746
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-15 22:09:37 +0900

    add Variable class

--- src/main/scala/core/Variable.scala
+++ src/main/scala/core/Variable.scala
@@ -1,5 +1,15 @@
 package main.scala.core
 
-class Variable {
+class Variable(val name: Option[String], var value: Option[Value]) extends Node {
+  def this(name: String) = this(Some(name), None)
+  def this(lexicalUnit: LexicalUnit) = this(None, Some(lexicalUnit.getValue.get))
 
+  def isMatch(environment: Environment, lexicalUnit: LexicalUnit): Option[Node] = {
+    if(lexicalUnit.getType == NAME) Some(new Variable(lexicalUnit.getValue.get.getString))
+    else None
+  }
+
+  def setValue(value: Value): Unit = this.value = Some(value)
+
+  def getValue(): Option[Value] = value
 }
