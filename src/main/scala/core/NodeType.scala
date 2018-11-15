adbdbbde3a850e242504883f3ccbe88c7a4f7db7
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-15 22:09:23 +0900

    add NodeType class

--- src/main/scala/core/NodeType.scala
+++ src/main/scala/core/NodeType.scala
@@ -1,5 +1,21 @@
 package main.scala.core
 
-class NodeType {
+abstract class NodeType
 
-}
+case object PROGRAM extends NodeType
+case object STMT_LIST extends NodeType
+case object STMT extends NodeType
+case object FOR_STMT extends NodeType
+case object SUBST_STMT extends NodeType
+case object BLOCK extends NodeType
+case object IF_BLOCK extends NodeType
+case object LOOP_BLOCK extends NodeType
+case object COND extends NodeType
+case object EXPR_LIST extends NodeType
+case object EXPR extends NodeType
+case object FUNCTION_CALL extends NodeType
+case object STRING_CONSTANT extends NodeType
+case object INT_CONSTANT extends NodeType
+case object DOUBLE_CONSTANT extends NodeType
+case object BOOL_CONSTANT extends NodeType
+case object END extends NodeType
